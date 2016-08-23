c5fb26dc0fc5160a2256018ada9e3303cb9d407b
Aad Versteden <madnificent@gmail.com>
2016-08-23 14:24:10 +0200

    Add call to copy between graphs in one database
    The POST /copy copies the contents from one graph to another in a single
    sparql endpoint.

--- web.rb
+++ web.rb
@@ -1,5 +1,30 @@
 # see https://github.com/mu-semtech/mu-ruby-template for more info
 get '/' do
   content_type 'application/json'
-  { data: { attributes: { hello: 'world' } } }
+  { data: { attributes: { hello: 'world' } } }.to_json
+end
+
+
+# Copies one graph to another
+#
+# Query parameters:
+# - source_graph: The graph to copy contents from
+# - target_graph: The graph to copy contents to
+post '/copy/' do
+  content_type 'application/json'
+
+  update <<SPARQL
+    INSERT {
+      GRAPH <#{params["target_graph"]}> {
+        ?s ?p ?o.
+      }
+    }
+    WHERE {
+      GRAPH <#{params["source_graph"]}> {
+        ?s ?p ?o.
+      }
+    }
+SPARQL
+
+  { data: { attributes: { status: "executed" } } }.to_json
 end
